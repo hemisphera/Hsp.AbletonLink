6907e519576400860fb8e414c3e6cfd72bbd7796
The Dumb <info@hemisphera.com>
2025-07-14 18:17:10 +0200

    Refactor

--- Hsp.AbletonLink/AbletonLink.cs
+++ Hsp.AbletonLink/AbletonLink.cs
@@ -3,168 +3,143 @@ using System.Runtime.InteropServices;
 
 namespace Hsp.AbletonLink;
 
-public class AbletonLink : IDisposable
+public sealed class AbletonLink : IDisposable
 {
-  private static volatile AbletonLink singletonInstance;
-  private IntPtr nativeInstance = IntPtr.Zero;
-  private const double INITIAL_TEMPO = 120.0;
-
-  public static AbletonLink Instance
-  {
-    get
-    {
-      if (singletonInstance == null)
-      {
-        singletonInstance = new AbletonLink();
-        singletonInstance.Setup(INITIAL_TEMPO);
-      }
-
-      return singletonInstance;
-    }
-  }
+  private readonly IntPtr _nativeInstance;
+  private const double InitialTempo = 120.0;
 
 
-  [DllImport("AbletonLinkDLL")]
+  [DllImport("link")]
   private static extern IntPtr CreateAbletonLink();
 
-  private AbletonLink()
-  {
-    nativeInstance = CreateAbletonLink();
-  }
-
-  ~AbletonLink()
-  {
-    this.Dispose();
-  }
-
-  [DllImport("AbletonLinkDLL")]
+  [DllImport("link")]
   private static extern void DestroyAbletonLink(IntPtr ptr);
+
+  [DllImport("link")]
+  private static extern void setup(IntPtr ptr, double bpm);
+
+  [DllImport("link")]
+  private static extern void setTempo(IntPtr ptr, double bpm);
+
+  [DllImport("link")]
+  private static extern double tempo(IntPtr ptr);
+
+  [DllImport("link")]
+  private static extern void setQuantum(IntPtr ptr, double quantum);
+
+  [DllImport("link")]
+  private static extern double quantum(IntPtr ptr);
+
+  [DllImport("link")]
+  private static extern void forceBeatAtTime(IntPtr ptr, double beat);
+
+  [DllImport("link")]
+  private static extern void requestBeatAtTime(IntPtr ptr, double beat);
+
+  [DllImport("link")]
+  private static extern void enableStartStopSync(IntPtr ptr, bool bEnable);
+
+  [DllImport("link")]
+  private static extern void startPlaying(IntPtr ptr);
+
+  [DllImport("link")]
+  private static extern int numPeers(IntPtr ptr);
+
+  [DllImport("link")]
+  private static extern bool isPlaying(IntPtr ptr);
+
+  [DllImport("link")]
+  private static extern void stopPlaying(IntPtr ptr);
+
+  [DllImport("link")]
+  private static extern void update(IntPtr ptr, out double rbeat, out double rphase, out double rtempo, out double rquantum, out double rtime, out int rnumPeers);
+
+  [DllImport("link")]
+  private static extern void enable(IntPtr ptr, bool bEnable);
+
+  [DllImport("link")]
+  private static extern bool isEnabled(IntPtr ptr);
+
+
+  public AbletonLink()
+  {
+    _nativeInstance = CreateAbletonLink();
+    Setup(InitialTempo);
+  }
 
   public void Dispose()
   {
-    singletonInstance = null;
-    if (nativeInstance != IntPtr.Zero)
-    {
-      DestroyAbletonLink(nativeInstance);
-      nativeInstance = IntPtr.Zero;
-    }
+    if (_nativeInstance == IntPtr.Zero) return;
+    DestroyAbletonLink(_nativeInstance);
   }
-
-  [DllImport("AbletonLinkDLL")]
-  private static extern void setup(IntPtr ptr, double bpm);
 
   private void Setup(double bpm)
   {
-    setup(nativeInstance, bpm);
+    setup(_nativeInstance, bpm);
   }
 
-  [DllImport("AbletonLinkDLL")]
-  private static extern void setTempo(IntPtr ptr, double bpm);
-
-
-  [DllImport("AbletonLinkDLL")]
-  private static extern double tempo(IntPtr ptr);
 
   public double Tempo
   {
-    get { return tempo(nativeInstance); }
-    set { setTempo(nativeInstance, value); }
+    get => tempo(_nativeInstance);
+    set => setTempo(_nativeInstance, value);
   }
-
-
-  [DllImport("AbletonLinkDLL")]
-  private static extern void setQuantum(IntPtr ptr, double quantum);
-
-
-  [DllImport("AbletonLinkDLL")]
-  private static extern double quantum(IntPtr ptr);
 
   public double Quantum
   {
-    get { return quantum(nativeInstance); }
-    set { setQuantum(nativeInstance, value); }
+    get => quantum(_nativeInstance);
+    set => setQuantum(_nativeInstance, value);
   }
-
-  [DllImport("AbletonLinkDLL")]
-  private static extern void forceBeatAtTime(IntPtr ptr, double beat);
 
   public void ForceBeatAtTime(double beat)
   {
-    forceBeatAtTime(nativeInstance, beat);
+    forceBeatAtTime(_nativeInstance, beat);
   }
 
-
-  [DllImport("AbletonLinkDLL")]
-  private static extern void requestBeatAtTime(IntPtr ptr, double beat);
 
   public void RequestBeatAtTime(double beat)
   {
-    requestBeatAtTime(nativeInstance, beat);
+    requestBeatAtTime(_nativeInstance, beat);
   }
 
-  [DllImport("AbletonLinkDLL")]
-  private static extern void enable(IntPtr ptr, bool bEnable);
-
-  [DllImport("AbletonLinkDLL")]
-  private static extern bool isEnabled(IntPtr ptr);
 
   public bool Enabled
   {
-    get { return isEnabled(nativeInstance); }
-    set { enable(nativeInstance, value); }
+    get => isEnabled(_nativeInstance);
+    set => enable(_nativeInstance, value);
   }
 
+  public bool IsPlaying => isPlaying(_nativeInstance);
 
-  [DllImport("AbletonLinkDLL")]
-  private static extern void enableStartStopSync(IntPtr ptr, bool bEnable);
+  public int NumPeers => numPeers(_nativeInstance);
+
 
   public void EnableStartStopSync(bool enable)
   {
-    enableStartStopSync(nativeInstance, enable);
+    enableStartStopSync(_nativeInstance, enable);
   }
-
-
-  [DllImport("AbletonLinkDLL")]
-  private static extern void startPlaying(IntPtr ptr);
 
   public void StartPlaying()
   {
-    startPlaying(nativeInstance);
+    startPlaying(_nativeInstance);
   }
-
-
-  [DllImport("AbletonLinkDLL")]
-  private static extern void stopPlaying(IntPtr ptr);
 
   public void StopPlaying()
   {
-    stopPlaying(nativeInstance);
+    stopPlaying(_nativeInstance);
   }
 
-
-  [DllImport("AbletonLinkDLL")]
-  private static extern bool isPlaying(IntPtr ptr);
-
-  public bool IsPlaying
+  public LinkState GetState()
   {
-    get { return isPlaying(nativeInstance); }
-  }
-
-
-  [DllImport("AbletonLinkDLL")]
-  private static extern int numPeers(IntPtr ptr);
-
-  public int NumPeers
-  {
-    get { return numPeers(nativeInstance); }
-  }
-
-
-  [DllImport("AbletonLinkDLL")]
-  private static extern void update(IntPtr ptr, out double rbeat, out double rphase, out double rtempo, out double rquantum, out double rtime, out int rnumPeers);
-
-  public void Update(out double beat, out double phase, out double tempo, out double quantum, out double time, out int numPeers)
-  {
-    update(nativeInstance, out beat, out phase, out tempo, out quantum, out time, out numPeers);
+    update(_nativeInstance, out var beat, out var phase, out var tempo, out var quantum, out var time, out var numPeers);
+    return new LinkState
+    {
+      Beat = beat,
+      Phase = phase,
+      Tempo = tempo,
+      Quantum = quantum,
+      Time = time,
+      NumPeers = numPeers
+    };
   }
 }
